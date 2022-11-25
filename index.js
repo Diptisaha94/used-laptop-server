dd89c4094762732b525c6bd85f4af3fe22e5baa7
Dipti saha <sahadipti322@gmail.com>
2022-11-24 22:52:38 -0500

    seller data filter and send

--- index.js
+++ index.js
@@ -21,11 +21,35 @@ async function run() {
     try {
         const categoryCollection = client.db('laptop').collection('laptopCategoryCollection');
         const laptopProductsCollection = client.db('laptop').collection('laptopProductsCollection');
+        const usersCollection=client.db('laptop').collection('usersCollection');
      app.get('/category',async(req,res)=>{
         const query = {};
         const result=await categoryCollection.find(query).toArray();
         res.send(result);
-     })
+     });
+     app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+  });
+  app.get('/users', async (req, res) => {
+    const query ={};
+    const result = await usersCollection.find(query).toArray();
+    res.send(result);
+});
+app.get('/users/admin/:email', async (req, res) => {
+  const email = req.params.email;
+  const query = { email }
+  const user = await usersCollection.findOne(query);
+  res.send({ isAdmin: user?.optionRole === 'admin' });
+});
+app.get('/users/seller/:email', async (req, res) => {
+  const email = req.params.email;
+  const query = { email }
+  const user = await usersCollection.findOne(query);
+  res.send({ isSeller: user?.optionRole === 'seller' });
+});
      app.get('/category/:id',async(req,res)=>{
         const id = req.params.id;
         const query = {category_id:id};
