e7e9cdb4f8e3a274b387a9ba4fd97963d2d8de07
Dipti saha <sahadipti322@gmail.com>
2022-11-26 20:04:45 -0500

    buyer order data send

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://travel:AdminTul@cluster0.yyxrk8i.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
@@ -50,6 +50,12 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
   });
+//   app.delete('/users/:id', async (req, res) => {
+//     const id = req.params.id;
+//     const query = {_id:ObjectId(id)}
+//     const result = await usersCollection.deleteOne(query);
+//     res.send(result);
+// });
   app.get('/users/admin/allsellers', async (req, res) => {
     //const email = req.params.email;
     const query = {optionRole : 'seller'}
@@ -57,7 +63,6 @@ async function run() {
     res.send(user);
   });
   app.get('/users/admin/allbuyers', async (req, res) => {
-    //const email = req.params.email;
     const query = {optionRole : 'buyer'}
     const user = await usersCollection.find(query).toArray();
     res.send(user);
@@ -97,7 +102,38 @@ app.get('/users/buyer/:email', async (req, res) => {
         const query = {category_id:id};
         const result=await laptopProductsCollection.find(query).toArray();
         res.send(result);
-     })
+     });
+     app.get('/myorders/:email',async(req,res)=>{
+      const email = req.params.email;
+      console.log(email);
+      const query = {email};
+      const result=await bookingCollection.find(query).toArray();
+      res.send(result);
+   });
+     app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await usersCollection.deleteOne(filter);
+      res.send(result);
+  });
+  app.delete('/myorders/:id', async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: ObjectId(id) };
+    const result = await bookingCollection.deleteOne(filter);
+    res.send(result);
+});
+  app.put('/users/seller/:id', async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: ObjectId(id) }
+    const options = { upsert: true };
+    const updatedDoc = {
+        $set: {
+            status: 'verify'
+        }
+    }
+    const result = await usersCollection.updateOne(filter, updatedDoc, options);
+    res.send(result);
+});
     //  app.get('/addType', async (req, res) => {
     //       const filter = {}
     //       const options = { upsert: true }
