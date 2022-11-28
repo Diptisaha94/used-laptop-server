dceb36c107c84c6fd2a3eee0c8e40ac68e74b7bc
Dipti saha <sahadipti322@gmail.com>
2022-11-28 08:19:54 -0500

    enverionment variable setup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 const port = process.env.port || 5000;
-
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
@@ -15,7 +15,7 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://travel:AdminTul@cluster0.yyxrk8i.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yyxrk8i.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
@@ -103,6 +103,11 @@ app.get('/users/buyer/:email', async (req, res) => {
         const result=await laptopProductsCollection.find(query).toArray();
         res.send(result);
      });
+     app.get('/allproduts',async(req,res)=>{
+      const query = {};
+      const result=await laptopProductsCollection.find(query).limit(3).toArray();
+      res.send(result);
+   });
      app.get('/myorders/:email',async(req,res)=>{
       const email = req.params.email;
       console.log(email);
@@ -110,6 +115,13 @@ app.get('/users/buyer/:email', async (req, res) => {
       const result=await bookingCollection.find(query).toArray();
       res.send(result);
    });
+   app.get('/myproducts/:email',async(req,res)=>{
+    const email = req.params.email;
+    console.log(email);
+    const query = {email};
+    const result=await laptopProductsCollection.find(query).toArray();
+    res.send(result);
+ });
      app.delete('/users/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) };
@@ -122,17 +134,34 @@ app.get('/users/buyer/:email', async (req, res) => {
     const result = await bookingCollection.deleteOne(filter);
     res.send(result);
 });
-  app.put('/users/seller/:id', async (req, res) => {
-    const id = req.params.id;
-    const filter = { _id: ObjectId(id) }
-    const options = { upsert: true };
-    const updatedDoc = {
-        $set: {
-            status: 'verify'
-        }
-    }
-    const result = await usersCollection.updateOne(filter, updatedDoc, options);
-    res.send(result);
+app.put('/users/seller/:email', async (req, res) => {
+  const email = req.params.email;
+  const filter = {email}
+  const options = { upsert: true };
+  const updatedDoc = {
+      $set: {
+          status: 'verify'
+      }
+  }
+  const result = await usersCollection.updateOne(filter, updatedDoc, options);
+  res.send(result);
+});
+app.put('/myproducts/:email', async (req, res) => {
+  const email = req.params.email;
+  const filter = {email}
+  const options = { upsert: true };
+  const updatedDoc = {
+      $set: {
+          status: 'verify'
+      }
+  }
+  const result = await laptopProductsCollection.updateOne(filter, updatedDoc, options);
+  res.send(result);
+});
+app.get('/users/sellerverify',async(req,res)=>{
+  const query = {status: 'verify'};
+  const result=await usersCollection.find(query).toArray();
+  res.send(result);
 });
     //  app.get('/addType', async (req, res) => {
     //       const filter = {}
